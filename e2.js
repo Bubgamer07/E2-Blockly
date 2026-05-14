a6db7db1bd9623282139927b9a8654c21e9cf9a7
Bubgamer07 <thhegamer07@gmail.com>
2026-05-14 07:26:01 -0700

    Add all the files n stuff idk

---
+++ e2.js
@@ -0,0 +1,44 @@
+$(function(){
+    // Create toolbox
+    var toolbox = {
+        "kind": "categoryToolbox",
+        "contents": [
+            {
+                "kind": "category",
+                "name": "Control",
+                "contents": [
+                    {
+                        "kind": "block",
+                        "type": "controls_if"
+                    },
+                ]
+            },
+            {
+                "kind": "category",
+                "name": "Logic",
+                "contents": [
+                    {
+                        "kind": "block",
+                        "type": "logic_compare"
+                    },
+                    {
+                        "kind": "block",
+                        "type": "logic_operation"
+                    },
+                    {
+                        "kind": "block",
+                        "type": "logic_boolean"
+                    }
+                ]
+            }
+        ]
+    };
+    // Inject Blockly into the Blockly div
+    const workspace = Blockly.inject('blocklyDiv',{
+        oneBasedIndex:true,
+        toolbox:toolbox
+    })
+    $('#generateCode').on('click',function(){
+        $('#generatedCode').val(javascriptGenerator.workspaceToCode(workspace))
+    })
+})
